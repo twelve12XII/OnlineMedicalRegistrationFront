485db882fda4d7763c7e22d69c8b4aebee96ae7d
MickeyShide <mickey_shide@list.ru>
2023-12-04 22:45:38 +0300

    asdasdasd

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,6 +2,8 @@ import { Link, useNavigate } from "react-router-dom";
 import norm_style from "../normalize.module.scss"
 import logo from '../../../public/logo.png'
 import pic from './assets/pic.png'
+import logo_light from '../../../public/logo_light.png'
+import background from '../../../public/background_1.png'
 
 export const Login = () => {
     let navigate = useNavigate();
@@ -13,11 +15,11 @@ export const Login = () => {
     return (
         <div className={norm_style.container}>
             <div className={norm_style.container_data}>
-                <img className={norm_style.logo} src={logo} alt="logo" />
+                <img className={norm_style.logo} src={logo_light} alt="logo" />
                 <div className={norm_style.container_blank}>
                     <form action="url">
                         <label>Номер полиса</label>
-                        <input type="text" />
+                        <input id={norm_style.first} type="text"/>
                         <label>Пароль</label>
                         <input type="password" />
                     </form>
@@ -27,10 +29,10 @@ export const Login = () => {
                     </div>
                     <button type="button" onClick={handleLogin}>Войти</button>
                 </div>
-                <p><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
+                <p id={norm_style.def_warning}><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
             </div>
             <div className={norm_style.container_image}>
-                <img src={pic} alt="pic"/>
+                <img id={norm_style.logo} src={logo_light} alt="logo"/>
             </div>
         </div>
     )
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -4,6 +4,8 @@ import { Modal } from '../../Interfaces/Modal/Modal'
 import norm_style from "../normalize.module.scss"
 import logo from '../../../public/logo.png'
 import pic from './assets/pic.png'
+import logo_light from '../../../public/logo_light.png'
+import background from '../../../public/background_1.png'
 
 export const SignUp = () => {
 
@@ -22,23 +24,23 @@ export const SignUp = () => {
     return (
         <div className={norm_style.container}>
             <div className={norm_style.container_data}>
-                <img className={norm_style.logo} src={logo} alt="logo" />
+                <img className={norm_style.logo} src={logo_light} alt="logo" />
                 <div className={norm_style.container_blank}>
                     <form action="url">
                         <label>Номер полиса</label>
-                        <input type="text" />
+                        <input id={norm_style.first} type="text" />
                         <label>Серия и номер паспорта</label>
                         <input type="text" />
                     </form>
-                    <div className={norm_style.container_links}>
+                    <div className={norm_style.container_blank__links}>
                         <Link to="/">Войти в аккаунт</Link>
                     </div>
                     <button type="button" onClick={() => setModal(true)}>Зарегистрироваться</button>
                 </div>
-                <p><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
+                <p id={norm_style.def_warning}><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
             </div>
             <div className={norm_style.container_image}>
-                <img src={pic} alt="pic" />
+                <img id={norm_style.logo} src={logo_light} alt="logo"/>
             </div>
             <Modal
                 visible={isModal}
