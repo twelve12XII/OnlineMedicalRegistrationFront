afc4c9b6dd2eb593193663a9bc6b4ae9180a4a39
twelve12XII <irishka.nikitina2002@mail.ru>
2023-12-05 16:22:46 +0300

    Revert "asdasdasd"
    This reverts commit 485db882fda4d7763c7e22d69c8b4aebee96ae7d.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,8 +2,6 @@ import { Link, useNavigate } from "react-router-dom";
 import norm_style from "../normalize.module.scss"
 import logo from '../../../public/logo.png'
 import pic from './assets/pic.png'
-import logo_light from '../../../public/logo_light.png'
-import background from '../../../public/background_1.png'
 
 export const Login = () => {
     let navigate = useNavigate();
@@ -15,11 +13,11 @@ export const Login = () => {
     return (
         <div className={norm_style.container}>
             <div className={norm_style.container_data}>
-                <img className={norm_style.logo} src={logo_light} alt="logo" />
+                <img className={norm_style.logo} src={logo} alt="logo" />
                 <div className={norm_style.container_blank}>
                     <form action="url">
                         <label>Номер полиса</label>
-                        <input id={norm_style.first} type="text"/>
+                        <input type="text" />
                         <label>Пароль</label>
                         <input type="password" />
                     </form>
@@ -29,10 +27,10 @@ export const Login = () => {
                     </div>
                     <button type="button" onClick={handleLogin}>Войти</button>
                 </div>
-                <p id={norm_style.def_warning}><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
+                <p><b>Внимание!</b><br />Никогда не публикуйте нигде свои логины и пароли и не называйте их никому, даже «администрации сайта».</p>
             </div>
             <div className={norm_style.container_image}>
-                <img id={norm_style.logo} src={logo_light} alt="logo"/>
+                <img src={pic} alt="pic"/>
             </div>
         </div>
     )
